bbfaca0a1861b87891928bc1b2dcfa2e990b4b12
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-09-07 02:32:47 -0300

    feat:containerHistorys-done

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
 import Nav from '@/components/nav/Nav'
 import styles from './page.module.css'
+import { Historys } from '@/components/Historys/Historys'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <div className={styles.main}>
       <Nav />
-    </main>
+      <div className={styles.hola}>
+        <Historys />
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas dignissimos blanditiis, fugit explicabo animi illo ullam, excepturi reprehenderit sequi commodi error quibusdam? Molestiae, eius. Perferendis soluta vero velit ipsa voluptatum!
+      </div>
+    </div>
   )
 }
---
+++ components/Historys/Historys.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+import styles from "./Historys.module.css"
+
+
+export const Historys = () => {
+  return (
+    <div className={styles.containerAll}>
+
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+
+        <div className={styles.containerHistory}>
+        <div className={styles.history}>
+          <img src="/images/Googleplay.png" alt="Googleplay" />
+        </div>
+        <span>Franco Chasro</span>
+        </div>
+    </div>
+  )
+}
