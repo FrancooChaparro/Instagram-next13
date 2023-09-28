befd5ab48870fff5347d3fa9eba7ce7589af204d
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-09-28 04:15:48 -0300

    clean_code

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,27 +1,73 @@
 import prisma from "@/app/lib/prismadb";
-import { NextResponse } from "next/server";
+import { User } from "@/app/types";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(
-  request: Request
-) {
-  const body = await request.json();
-  const {
-    email,
-    name,
-    image,
-    username,
-  } = body;
+export async function POST(request: NextRequest) {
+  const regexName = /^([a-zA-Z ]+)$/i;
+  const regexPassword = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{1,12}$/;
+  const regexEmail = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-  console.log("llego???");
+  try {
+    const body = await request.json();
+    const infoUser = {
+      email: "",
+      name: "",
+      username: "",
+      password: "",
+    };
+    const { email, name, image, username, password } = body;
 
-  const user = await prisma.user.create({
-    data: {
-      email,
-      name,
-      username,
-      image
+    if (!name || !password || !email || !username)
+      return NextResponse.json({ msg: "Missing required fields" });
+
+    if (email && email.length > 0 && email != "") {
+      if (regexEmail.test(email)) {
+        const userBD = await prisma.user.findUnique({
+          where: { email: `${email}` },
+        });
+        if (userBD) {
+          return NextResponse.json({ msg: "The email already exists" });
+        } else {
+          infoUser.email = `${email}`;
+        }
+      }
     }
-  });
 
-  return NextResponse.json(user);
+    if (name && name.length > 0 && name != "") {
+      if (regexName.test(name)) {
+        infoUser.name = `${name}`;
+      } else {
+        return NextResponse.json({ msg: "The name is invalid" });
+      }
+    }
+
+    if (username && username.length > 0 && username != "") {
+      if (regexName.test(username)) {
+        infoUser.username = `${username}`;
+      } else {
+        return NextResponse.json({ msg: "The name is invalid" });
+      }
+    }
+
+    if (password && password.length > 0 && password != "") {
+      if (regexPassword.test(password)) {
+        infoUser.password = password;
+      } else {
+        return NextResponse.json({ msg: "The password is invalid" });
+      }
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        email,
+        name,
+        username,
+        image,
+        password,
+      },
+    });
+    return NextResponse.json({status: true, user: user});
+  } catch (error) {
+    return NextResponse.json({ msg: `Error 404 - ${error}` });
+  }
 }
--- app/api/see_follows/[...id]/route.ts
+++ app/api/see_follows/[...id]/route.ts
@@ -1,66 +1,67 @@
 import prisma from "@/app/lib/prismadb";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { parse } from "url";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   const { pathname } = parse(request.url || "", true);
   const [, id] = (pathname || "").split("api/see_follows/");
 
-  if (!id) {
-    return new NextResponse(
-      JSON.stringify({ message: "User not found" }),
-      {
-        status: 400,
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-  }
-
-  const userID = await prisma.user.findUnique({
-    where: {
-      id: parseInt(id, 10),
-    },
-  });
-
-  if (!userID) {
-    return new NextResponse(
-      JSON.stringify({ message: "user not found" }),
-      {
-        status: 404,
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-  }
-  let seguidos: any[] = [];  // Inicializa la variable con un array vacío
-  let arrayseguidos = userID.following;
-
-  for (const e of arrayseguidos) {
-    let user = await prisma.user.findUnique({
-      where: {
-        id: e,
-      },
-    });
-    seguidos.push(user);
-  }
-
-  let seguidores: any[] = [];  // Inicializa la variable con un array vacío
-  let arrayFollows = userID.followers;
-
-  for (const e of arrayFollows) {
-    let user = await prisma.user.findUnique({
-      where: {
-        id: e,
-      },
-    });
-    seguidores.push(user);
-  }
-
-  return new NextResponse(
-    JSON.stringify({ userID, seguidores, seguidos }),
-    {
-      status: 200,
-      headers: { "Content-Type": "application/json" },
+  try {
+    if (isNaN(parseInt(id, 10))) {
+      return NextResponse.json(
+        { status: false, msg: "Invalid user ID provided" },
+        { status: 400 }
+      );
     }
-  );
-}
+    const userID = await prisma.user.findUnique({
+      where: {
+        id: parseInt(id, 10),
+      },
+      include: {
+        posts: true,
+        like: {
+          include: {
+            post: true,
+          },
+        },
+      },
+    });
 
+    if (!userID) {
+      return NextResponse.json(
+        { status: false, msg: "User not found" },
+        { status: 404 }
+      );
+    }
+
+    let seguidos: any[] = [];
+    let arrayseguidos = userID.following;
+
+    for (const e of arrayseguidos) {
+      let user = await prisma.user.findUnique({
+        where: {
+          id: e,
+        },
+      });
+      seguidos.push(user);
+    }
+
+    let seguidores: any[] = [];
+    let arrayFollows = userID.followers;
+
+    for (const e of arrayFollows) {
+      let user = await prisma.user.findUnique({
+        where: {
+          id: e,
+        },
+      });
+      seguidores.push(user);
+    }
+    return NextResponse.json(
+      { status: true, user: userID, seguidores, seguidos },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json({ msg: `Error 404 - ${error}` });
+  }
+}
