a1bb4dc9ce049f9690f0413175d95ddd81550166
FrancooChaparro <francoo_chaparro@hotmail.com>
2023-11-09 22:23:51 -0300

    testing

--- app/page.tsx
+++ app/page.tsx
@@ -23,75 +23,93 @@ async function getPosts() {
 
 export default function Home() {
   const [posts, setPosts] = useState([
-    // {
-    //   id: 1,
-    //   title: "Deadpool",
-    //     image: "/images/marvel3.avif",
-    //   liked: false,
-    //   authorId: 6,
-    //   author: {
-    //     id: 13,
-    //         email: "marvel@hotmail.com",
-    //         name: "Marvel Studios",
-    //         username: "marvel",
-    //         password: "$2b$10$0wgBc0vgwAnxf73gPHwKouH/48tzVbeVLXMy0PnjLvSo2rbvyTTp.",
-    //         image: "/images/marvel.jpg",
-    //     followers: [],
-    //     following: [1, 5, 7, 8, 4, 2, 9, 10, 11],
-    //   },
-    //   comments: [],
-    //   likes: [1,1,1],
-    // },
-    // {
-    //   id: 2,
-    //   title: "Background Marvel",
-    //   image: "/images/marvel2.jpeg",
-    //   liked: false,
-    //   authorId: 6,
-    //   author: {
-    //     id: 13,
-    //         email: "marvel@hotmail.com",
-    //         name: "Marvel Studios",
-    //         username: "marvel",
-    //         password: "$2b$10$0wgBc0vgwAnxf73gPHwKouH/48tzVbeVLXMy0PnjLvSo2rbvyTTp.",
-    //         image: "/images/marvel.jpg",
-    //     followers: [],
-    //     following: [1, 5, 7, 8, 4, 2, 9, 10, 11],
-    //   },
-    //   comments: [],
-    //   likes: [1],
-    // },
-    // {
-    //   id: 3,
-    //   title: "",
-    //   image: "/images/marvel4.jpg",
-    //   liked: false,
-    //   authorId: 6,
-    //   author: {
-    //     id: 13,
-    //         email: "marvel@hotmail.com",
-    //         name: "Marvel Studios",
-    //         username: "marvel",
-    //         password: "$2b$10$0wgBc0vgwAnxf73gPHwKouH/48tzVbeVLXMy0PnjLvSo2rbvyTTp.",
-    //         image: "/images/marvel.jpg",
-    //     followers: [],
-    //     following: [1, 5, 7, 8, 4, 2, 9, 10, 11],
-    //   },
-    //   comments: [],
-    //   likes: [1,2,3,4,5,6,7,8,9,10,11,12],
-    // },
+     {
+       id: 1,
+       title: "Deadpool",
+         image: "/images/marvel3.avif",
+       liked: false,
+       authorId: 6,
+       author: {
+         id: 13,
+             email: "marvel@hotmail.com",
+             name: "Marvel Studios",
+             username: "marvel",
+             password: "$2b$10$0wgBc0vgwAnxf73gPHwKouH/48tzVbeVLXMy0PnjLvSo2rbvyTTp.",
+             image: "/images/marvel.jpg",
+         followers: [],
+         following: [1, 5, 7, 8, 4, 2, 9, 10, 11],
+       },
+       comments: [  {
+                id: 21,
+                content: "awesome pic",
+                authorIdComent: 14,
+                comentPostId: 10,
+                author: {
+                    id: 14,
+                    email: "bill@hotmail.com",
+                    name: "Bill Gates",
+                    username: "billgates",
+                    password: "$2b$10$iuvjqTzdqB5NKUL0ptfj0OgNd0AZVqwLFHdkny8CkewdHo7XlHL4q",
+                    image: "/images/bill_gates.jpg",
+                    followers: [
+                        16,
+                        13
+                    ],
+                    "following": []
+                }
+            }],
+       likes: [1,1,1],
+     },
+     {
+       id: 2,
+       title: "Background Marvel",
+       image: "/images/marvel2.jpeg",
+       liked: false,
+       authorId: 6,
+       author: {
+         id: 13,
+             email: "marvel@hotmail.com",
+             name: "Marvel Studios",
+             username: "marvel",
+             password: "$2b$10$0wgBc0vgwAnxf73gPHwKouH/48tzVbeVLXMy0PnjLvSo2rbvyTTp.",
+             image: "/images/marvel.jpg",
+         followers: [],
+         following: [1, 5, 7, 8, 4, 2, 9, 10, 11],
+       },
+       comments: [],
+       likes: [1],
+     },
+     {
+       id: 3,
+       title: "",
+       image: "/images/marvel4.jpg",
+       liked: false,
+       authorId: 6,
+       author: {
+         id: 13,
+             email: "marvel@hotmail.com",
+             name: "Marvel Studios",
+             username: "marvel",
+             password: "$2b$10$0wgBc0vgwAnxf73gPHwKouH/48tzVbeVLXMy0PnjLvSo2rbvyTTp.",
+             image: "/images/marvel.jpg",
+         followers: [],
+         following: [1, 5, 7, 8, 4, 2, 9, 10, 11],
+       },
+       comments: [],
+       likes: [1,2,3,4,5,6,7,8,9,10,11,12],
+     },
   ]);
   const { black_mode } = useMyContext();
 
-  useEffect(() => {
-    getPosts()
-      .then((data) => {
-        setPosts(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   getPosts()
+  //     .then((data) => {
+  //       setPosts(data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
   return (
     <div className={styles.main}>
